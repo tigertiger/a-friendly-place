05a1fe3a7648e0595df1bad6abb9441022a38d2d
Melissa Schatz-Miller <melissa.scmi@gmail.com>
2024-05-21 10:11:52 -0700

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hullo World!')
 })
 
+app.get('/api', (req, res) => {
+  res.send('Hullo, World! Good Night, Moon.')
+})
+
 app.listen(port, () => {
   console.log(`Friendly app listening on port ${port}`)
-}) 
+})
